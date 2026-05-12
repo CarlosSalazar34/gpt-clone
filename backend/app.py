1935d8eaff3f77d10552e1316f3978aedd4f4e29
CarlosSalazar34 <carloseliassalazaryunes@gmail.com>
2026-05-12 08:24:46 -0400

    add the execution of the server

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,5 @@
+from socket import socket
+from httpx import __name
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from openai import OpenAI
@@ -40,3 +42,8 @@ async def chat(request: Request):
 
     except Exception as e: 
         return {"error": str(e)}
+
+if __name__ == "__main__":
+    import uvicorn
+    import socket
+    uvicorn.run(app="app:app", host=f"{socket.gethostbyname(socket.gethostname())}", port=5000, reload=True)
